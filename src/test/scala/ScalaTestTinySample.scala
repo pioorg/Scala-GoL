9edcd76970e46676ec61fa3baa39acfac917123f
Piotr <piotr@przybyl.org>
2020-02-25 18:02:16 +0100

    Added samples for usage of Scala Specs2 and Scala Test.

---
+++ src/test/scala/ScalaTestTinySample.scala
@@ -0,0 +1,33 @@
+/*
+ *  Copyright (C) 2020 Piotr Przybył
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+
+class SampleFlatSpec extends AnyFlatSpec with Matchers{
+  "one" should "be less than two" in {
+    1 should be < 2
+  }
+}
+
+class SampleFunSpec extends AnyFunSuite with Matchers{
+  test("three should be more than two") {
+    assert(3 > 2)
+  }
+}
